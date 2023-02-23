924bae677572e01259f10aec3a41c5ca99ea93a1
Jansi1994 <jansiramm@gmail.com>
2023-02-23 11:57:10 +0530

    Sample Work

---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,17 @@
+package org.bank;
+
+public class BankInfo extends AxisBank  {
+	public void saving() {
+		System.out.println("saving : 30");
+		}
+	public void fixed() {
+		System.out.println("fixed : 20");
+	}
+
+	public static void main(String[] args) {
+		BankInfo data = new BankInfo();
+		data.saving();
+		data.fixed();
+		data.deposit();
+	}
+}
